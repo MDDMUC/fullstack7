28fb2cf90d8b0fc99dfcb4bda6a37a498def0580
MDDMUC <hello@martindrexler.com>
2025-11-23 22:32:28 +0100

    new render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,9 @@
-'use client'
-
-import Image from "next/image";
-import { useEffect } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import ClimberList from '@/components/climberlist'
 
 export default function Home() {
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data, error } = await supabase.from('profiles').select();
-      console.log('data:', data);
-      console.log('error:', error);
-    };
-    fetchData();
-  }, []);
-
   return (
-    <main className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black p-10 text-xl">
-      <div className="max-w-3xl space-y-6">
-        <h1 className="text-4xl font-bold">Welcome to Your Dating App</h1>
-        <p>Check your browser console for Supabase output 👀</p>
-        {/* You can keep the Next.js starter images or replace them: */}
-        <Image
-          src="/vercel.svg"
-          alt="Vercel Logo"
-          width={100}
-          height={24}
-        />
-      </div>
+    <main className="bg-white dark:bg-black min-h-screen">
+      <ClimberList />
     </main>
-  );
+  )
 }
